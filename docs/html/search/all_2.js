6222f0d3996d1f27251df8939149c79bae1ccc7d
OmarSuarezDoro <alu0101483474@ull.edu.es>
2024-07-19 13:05:44 +0200

    Upload Documentation

---
+++ docs/html/search/all_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['operator_28_29_2',['operator()',['../classMatrix.html#a8ac87a472fea65c216943a72f43babe2',1,'Matrix::operator()(unsigned int index_rows, unsigned int index_cols) const'],['../classMatrix.html#a66967d5fae3c7fb21c1c753f72bb60a8',1,'Matrix::operator()(unsigned int index_rows, unsigned int index_cols)']]],
+  ['operator_2b_3',['operator+',['../classMatrix.html#a0deb8db244c907e441b295cb3f8a2610',1,'Matrix::operator+()'],['../classPolynomial.html#a8b9a213388d9f15d50f5c1324a5c66a1',1,'Polynomial::operator+(const Polynomial&lt; T &gt; &amp;kPolynomial2) const']]],
+  ['operator_5b_5d_4',['operator[]',['../classPolynomial.html#af3d243cdf6373cfd64cc97e5c4417765',1,'Polynomial::operator[](int index) const'],['../classPolynomial.html#ae16b2c25af57ff83ef9e46346eb0da1f',1,'Polynomial::operator[](int index)']]]
+];
---
+++ docs/html/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['polynomial_7',['Polynomial',['../classPolynomial.html',1,'']]]
+];
