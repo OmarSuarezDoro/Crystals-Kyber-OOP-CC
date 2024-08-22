ca517d9fb33c8417b4fd025f572dca2d9ef8e6e0
OmarSuarezDoro <alu0101483474@ull.edu.es>
2024-08-22 18:48:19 +0200

    Changed header in client

--- src/client.cc
+++ src/client.cc
@@ -1,3 +1,15 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Trabajo de Fin de Grado - Kleptographic Attacks on Lattice-Based CryptoSystems
+ *
+ * @author Omar Suárez Doro
+ * @date August 22 2024
+ * @version v0.1.0
+ * @brief This is the client program that uses the Kyber cryptosystem
+ */
+
 #include <iostream>
 #include <string>
 
