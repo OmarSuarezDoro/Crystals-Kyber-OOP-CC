193caa72593e84f6eacf30e46a050b9ca1b5f100
OmarSuarezDoro <alu0101483474@ull.edu.es>
2024-10-04 14:44:55 +0100

    Updates

--- src/Attack/KleptoKyber.cc
+++ src/Attack/KleptoKyber.cc
@@ -47,23 +47,33 @@ Bytes KleptoKyber::RunBackdoor() {
       p[i] += bit_j * (1 << (j - i * c));
     }
   }
-  // std::cout << "Polynomial p: " << p << std::endl;
   Polynomial<int> h(p.GetSize());  
   // Transform the matrix t into a polynomial to calculate the compensation polynomial
   Polynomial<int> t_polynomial = t.GetPolynomialFromMatrix(n_);
   // Compute the compensation polynomial
+    // h[i] = std::abs((p[i] - t_polynomial[i]) % (2 << c - 1)) ;
   for (int i = 0; i < p.GetSize(); ++i) {
-    h[i] = (p[i] - t_polynomial[i]) % c;
+    h[i] = (p[i] - t_polynomial[i])  % (2 << c - 1);
   }
   // std::cout << "Polynomial h: " << h << std::endl << std::endl;
   Polynomial<int> t_prime = t_polynomial + h;
+  // std::cout << t_prime << std::endl;
+  
+  Polynomial<int> copy = t_prime;
+  for (int i = 0; i < copy.GetSize(); ++i) {
+    copy[i] = copy[i] % (2 << c - 1);
+  } 
+  // std::cout << copy << std::endl;
+  if (copy == p) {
+    std::cout << "SIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+  }
   Matrix<Polynomial<int>> t_prime_matrix(k_, 1);
   int current_row = 0;
+
   for (int i = 0; i < t_prime.GetSize(); i += n_) {
     t_prime_matrix(current_row, 0) = ntt_->NTT_Kyber(t_prime.GetSubPolynomial(i, i + n_), true);
     current_row++;
   }
-  // std::cout << "Matrix t_prime: " << t_prime_matrix << std::endl;
   return seed_a + encdec_unit_->EncodeMatrixToBytes(t_prime_matrix, 12);
 }
 
@@ -85,18 +95,21 @@ void KleptoKyber::recoverSecretKey(const Bytes& pk) {
   // std::cout << "size_of_p_without_zeros: " << size_of_p_without_zeros << std::endl;
 
   // Reconstruir el polinomio p a partir de t_prime
-  int size_of_p_without_zeros = b / c; // Number of coefficients in p without appended zeros
+  int size_of_p_without_zeros = b / c + 1; // Number of coefficients in p without appended zeros
   Polynomial<int> p(k_ * n_);
   for (int i = 0; i < size_of_p_without_zeros; ++i) {
-    p[i] = t_prime_polynomial[i] % c;
+    p[i] = t_prime_polynomial[i] % (2 << c - 1);
   }
+  // std::cout << "Recovered Polynomial p: " << p << std::endl;  ---- P is well calculated!!!
 
   std::string recovered_ct_str = "";
-  for (int i = 0; i < p.GetSize(); ++i) {
-    recovered_ct_str += std::to_string(p[i]);
+  for (int i = 0; i < (size_of_p_without_zeros - 1); ++i) {
+    recovered_ct_str += std::bitset<2>(p[i]).to_string();
   }
-  Bytes recovered_ct = seed_a + Bytes::FromBitsToBytes(recovered_ct_str);
-  std::cout << "Recovered CT: " << recovered_ct.FromBytesToHex() << std::endl;
+  Bytes recovered_ct = Bytes::FromBitsToBytes(recovered_ct_str);
+
+  std::cout << "Recovered Ciphertext: " << recovered_ct.FromBytesToHex() << std::endl;
+
 
   Bytes m_prime = cypher_box->Decrypt(recovered_ct);
   std::pair<Matrix<Polynomial<int>>, int> result_sk_n = sampling_unit_->GenerateDistribuitionMatrix(m_prime, n1_, 0);
